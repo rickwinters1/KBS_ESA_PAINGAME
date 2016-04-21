bd89708769f50c29026f1f43d2cdee9e9c58645b
MikeSchotman <mikeschotman@gmail.com>
2016-04-21 13:22:27 +0200

    nog verder met menu

--- Menu.c
+++ Menu.c
@@ -5,8 +5,8 @@
 void menu(void* pdata){
 	int ID = (int*)pdata;
 	int gameModeMenu = 1;
-	int xMenu = 30;
-	int yMenu = 25;
+	int xMenu = 32;
+	int yMenu = 28;
 
 	if (controller(ID) == 1){
 		gameModeMenu+1;
@@ -21,6 +21,12 @@ void menu(void* pdata){
 
 	if (gameModeMenu <= 0){
 		gameModeMenu = 4;
+	} else if (gameModeMenu >= 5){
+		gameModeMenu = 1;
+	}
+	
+	/*if (gameModeMenu <= 0){
+		gameModeMenu = 4;
 	} else if (gameModeMenu == 1){
 		selecteerMenu();
 	} else if (gameModeMenu == 2){
@@ -31,7 +37,7 @@ void menu(void* pdata){
 		
 	} else if (gameModeMenu >= 5){
 		gameModeMenu = 1;
-	}
+	}*/
 
 
 }
@@ -40,16 +46,31 @@ void menu(void* pdata){
 void selecteerMenu(){
 	short menuTextKleur = 0xffff00;
 	
-	int xLinks = 140;
-	int xRechts = 240;
-	int xBoven = 100;
-	int xOnder = 150;
-
+	int xLinks = 100;
+	int xRechts = 180;
+	int xBoven = 70;
+	int xOnder = 90;
+	//  VGA_box(x1, y1, x2, y2, menuTextKleur);
+	if (gameModeMenu == 1){
+		VGA_box(xLinks, xBoven, xRechts, xBoven + 1, menuTextKleur); // bovenste
+		VGA_box(xLinks, xOnder, xRechts, xOnder + 1, menuTextKleur); // onderste
+		VGA_box(xLinks, xBoven, xLinks + 1, xOnder, menuTextKleur); // linkse
+		VGA_box(xRechts, xBoven, xRechts + 1, xOnder, menuTextKleur); // rechtse
+	} else if (gameModeMenu == 2){
+		VGA_box(xLinks, xOnder, xRechts, xOnder + 1, menuTextKleur); // bovenste
+		VGA_box(xLinks, xOnder + 20, xRechts, xOnder + 1 + 20, menuTextKleur); // onderste
+		VGA_box(xLinks, xBoven + 20, xLinks + 1 , xOnder + 20, menuTextKleur); // linkse
+		VGA_box(xRechts, xBoven + 20, xRechts + 1, xOnder + 20, menuTextKleur); // rechtse
+	} else if (gameModeMenu == 3){
+		VGA_box(xLinks, xOnder + 20, xRechts, xOnder + 1 + 20, menuTextKleur); // bovenste
+		VGA_box(xLinks, xOnder + 40, xRechts, xOnder + 1 + 40, menuTextKleur); // onderste
+		VGA_box(xLinks, xBoven + 40, xLinks + 1 , xOnder + 40, menuTextKleur); // linkse
+		VGA_box(xRechts, xBoven + 40, xRechts + 1, xOnder + 40, menuTextKleur); // rechtse
+	} else if (gameModeMenu == 4){
+		VGA_box(xLinks, xOnder + 40, xRechts, xOnder + 1 + 40, menuTextKleur); // bovenste
+		VGA_box(xLinks, xOnder + 60, xRechts, xOnder + 1 + 60, menuTextKleur); // onderste
+		VGA_box(xLinks, xBoven + 60, xLinks + 1 , xOnder + 60, menuTextKleur); // linkse
+		VGA_box(xRechts, xBoven + 60, xRechts + 1, xOnder + 60, menuTextKleur); // rechtse
+	}
 	
-
-	VGA_box(xLinks, xBoven, xRechts, xBoven + 1, menuTextKleur); // bovenste
-	VGA_box(xLinks, xOnder, xRechts, xOnder + 1, menuTextKleur); // onderste
-	VGA_box(xLinks, xBoven, xLinks + 1, xOnder, menuTextKleur); // linkse
-	VGA_box(xRechts, xBoven, xRechts + 1, xOnder, menuTextKleur); // rechtse
-	printf("AN PRINTEN");
 }
