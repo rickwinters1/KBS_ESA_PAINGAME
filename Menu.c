608f74bf4760dd637bed5ca954fbd72bd994da85
MikeSchotman <mikeschotman@gmail.com>
2016-04-25 12:12:38 +0200

    Betere code Menu

--- Menu.c
+++ Menu.c
@@ -3,8 +3,18 @@
 #include "altera_up_avalon_parallel_port.h"
 
 int gameModeMenu = 1;
-
+int vorige = 0;
 int controller(int ID);
+
+short menuTextKleur = 0xffff00;
+
+int xLinks = 100;
+int xRechts = 180;
+int xBoven = 70;
+int xOnder = 90;
+
+// 0xffff00 = wit
+// 0x000000 = zwart
 
 void menu(void* pdata){
 	int ID = (int*)pdata;
@@ -30,47 +40,47 @@ void menu(void* pdata){
 		selecteerMenu();
 		OSTimeDly(1);
 	}
-	VGA_text (xMenu, yMenu, "Singeplayer");
+	VGA_text (xMenu, yMenu, "Singleplayer");
 	VGA_text (xMenu, yMenu+4, "Multiplayer");
 	VGA_text (xMenu, yMenu+8, "Highscores");
 	VGA_text (xMenu, yMenu+12, "Tutorial");
-
-
-
-
-
-
 }
 
 
 void selecteerMenu(){
-	short menuTextKleur = 0xffff00;
-	
-	int xLinks = 100;
-	int xRechts = 180;
-	int xBoven = 70;
-	int xOnder = 90;
+
 	//  VGA_box(x1, y1, x2, y2, menuTextKleur);
 	if (gameModeMenu == 1){
-		VGA_box(xLinks, xBoven, xRechts, xBoven + 1, menuTextKleur); // bovenste
-		VGA_box(xLinks, xOnder, xRechts, xOnder + 1, menuTextKleur); // onderste
-		VGA_box(xLinks, xBoven, xLinks + 1, xOnder, menuTextKleur); // linkse
-		VGA_box(xRechts, xBoven, xRechts + 1, xOnder, menuTextKleur); // rechtse
+		tekenBox(xLinks, xBoven, xRechts, xOnder, 0xffff00);
+		tekenBox2(Links, Boven, Rechts, Onder, 0x000000);
+		VGA_text (xMenu, yMenu, "Singleplayer");
 	} else if (gameModeMenu == 2){
-		VGA_box(xLinks, xOnder, xRechts, xOnder + 1, menuTextKleur); // bovenste
-		VGA_box(xLinks, xOnder + 20, xRechts, xOnder + 1 + 20, menuTextKleur); // onderste
-		VGA_box(xLinks, xBoven + 20, xLinks + 1 , xOnder + 20, menuTextKleur); // linkse
-		VGA_box(xRechts, xBoven + 20, xRechts + 1, xOnder + 20, menuTextKleur); // rechtse
+		tekenBox(xLinks, xBoven+20, xRechts, xOnder+20, 0xffff00);
+		tekenBox2(Links, Boven+20, Rechts, Onder+20, 0x000000);
+		VGA_text (xMenu, yMenu+4, "Multiplayer");
 	} else if (gameModeMenu == 3){
-		VGA_box(xLinks, xOnder + 20, xRechts, xOnder + 1 + 20, menuTextKleur); // bovenste
-		VGA_box(xLinks, xOnder + 40, xRechts, xOnder + 1 + 40, menuTextKleur); // onderste
-		VGA_box(xLinks, xBoven + 40, xLinks + 1 , xOnder + 40, menuTextKleur); // linkse
-		VGA_box(xRechts, xBoven + 40, xRechts + 1, xOnder + 40, menuTextKleur); // rechtse
+		tekenBox(xLinks, xBoven+40, xRechts, xOnder+40, 0xffff00);
+		tekenBox2(Links, Boven+40, Rechts, Onder+40, 0x000000);
+		VGA_text (xMenu, yMenu+8, "Highscores");
 	} else if (gameModeMenu == 4){
-		VGA_box(xLinks, xOnder + 40, xRechts, xOnder + 1 + 40, menuTextKleur); // bovenste
-		VGA_box(xLinks, xOnder + 60, xRechts, xOnder + 1 + 60, menuTextKleur); // onderste
-		VGA_box(xLinks, xBoven + 60, xLinks + 1 , xOnder + 60, menuTextKleur); // linkse
-		VGA_box(xRechts, xBoven + 60, xRechts + 1, xOnder + 60, menuTextKleur); // rechtse
+		tekenBox(xLinks, xBoven+60, xRechts, xOnder+60, 0xffff00);
+		tekenBox2(Links, Boven+60, Rechts, Onder+60, 0x000000);
+		VGA_text (xMenu, yMenu+12, "Tutorial");
 	}
 	
+}
+
+void tekenBox(int Links, int Boven, int Rechts, int Onder, short Kleur){
+	
+	VGA_box(Links, Boven, Rechts, Onder, Kleur);
+	
+}
+
+void tekenBox2(int Links, int Boven, int Rechts, int Onder, short Kleur){
+	Links = xLinks - 1;
+	Boven = xBoven - 1;
+	Rechts = xRechts - 1;
+	Onder = xOnder -1;
+	
+	VGA_box(Links, Boven, Rechts, Onder, Kleur);
 }
