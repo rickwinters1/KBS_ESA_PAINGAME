fbf5126ae9577d100e3d35a745a4ffe2e551d02f
MikeSchotman <mikeschotman@gmail.com>
2016-04-28 11:39:47 +0200

    bal

--- KBS_ESA_PAINGAME/Game.c
+++ KBS_ESA_PAINGAME/Game.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
 #include "includes.h"
-#include "altera_up_avalon_parallel_port.h"
-
-short kleur1 = 0xffffff;
 
 int ALT_x1; int ALT_x2; int ALT_y;
 int ALT_inc_x; int ALT_inc_y;
-int char_buffer_x; int char_buffer_y;
+int pixel_buffer_x; int pixel_buffer_y;
 int blue_x1; int blue_y1; int blue_x2; int blue_y2;
+short balZwart = 0x000000;
+short balWit = 0xffffff;
 
 void Game(void* pdata){
-	ALT_x1 = 0; ALT_x2 = 5; ALT_y = 0; ALT_inc_x = 1; ALT_inc_y = 1;
+	
+	ALT_x1 = 0; ALT_x2 = 20; ALT_y = 0; ALT_inc_x = 1; ALT_inc_y = 1;
 
-	char_buffer_x = 79; char_buffer_y = 59;
+	pixel_buffer_x = 319; pixel_buffer_y = 239;
 
 	blue_x1 = 28; blue_x2 = 52; blue_y1 = 26; blue_y2 = 34;
+	
+	//ALT_x1 = 0; ALT_x2 = 5/* ALTERA = 6 chars */; ALT_y = 0; ALT_inc_x = 1; ALT_inc_y = 1;
 
+	//char_buffer_x = 79; char_buffer_y = 59;
+
+	//blue_x1 = 28; blue_x2 = 52; blue_y1 = 26; blue_y2 = 34;
 	while(1){
+		
+		VGA_box(ALT_x1, ALT_y, ALT_x1+5, ALT_y+5, zwart); // erase
+		ALT_x1 += ALT_inc_x;
+		ALT_x2 += ALT_inc_x;
+		ALT_y += ALT_inc_y;
 
+		if ( (ALT_y == pixel_buffer_y) || (ALT_y == 0) )
+			ALT_inc_y = -(ALT_inc_y);
+		if ( (ALT_x2 == pixel_buffer_x) || (ALT_x1 == 0) )
+			ALT_inc_x = -(ALT_inc_x);
+
+
+	
+		VGA_box(ALT_x1, ALT_y, ALT_x1+5, ALT_y+5, wit); // ball
+		
+		/*
 		VGA_text (ALT_x1, ALT_y, "          ");		// erase
 		ALT_x1 += ALT_inc_x;
 		ALT_x2 += ALT_inc_x;
@@ -31,6 +51,7 @@ void Game(void* pdata){
 
 
 		VGA_text (ALT_x1, ALT_y, "HALLOOO");
+		*/
 
 		OSTimeDly(1);
 	}
