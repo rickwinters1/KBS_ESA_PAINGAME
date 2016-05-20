2626e58b3eda554b217ade417c01adef40a2d3cb
MikeSchotman <mikeschotman@gmail.com>
2016-05-20 14:30:03 +0200

    dfsafdsasd

--- KBS_ESA_PAINGAME/Tutorial.c
+++ KBS_ESA_PAINGAME/Tutorial.c
@@ -1,1 +1,16 @@
-//balbalblalbal
+#include <stdio.h>
+#include "includes.h"
+
+OS_EVENT* gameSem;
+OS_EVENT* controllerSem;
+OS_EVENT* MailBox;
+OS_EVENT* MailBox2;
+
+
+OS_FLAG_GRP *Flags;
+
+#define Menu_Flag 0x01
+#define Game_Flag 0x02
+#define C1_Flag 0x04
+#define C2_Flag 0x08
+#define Menu2_Flag 0x10
